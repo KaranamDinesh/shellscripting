8269596a6aa54bb1720fbeda77d3a8831d07f9ad
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2020-09-30 13:19:06 +0530

    if else

--- first.sh
+++ first.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 a=$1
-
-if [ $a -gt 10 ]; then
-  echo a is greater than 10
+if [$a -gt 10];then
+  echo a is equal to zero
 else
-  echo a is less than or equal to 10
+  echo a is not equal to zero
 fi
